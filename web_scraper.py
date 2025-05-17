95f3d0887aee71f429b84edf36c41feaf5d1b32f
PenguinPush <icyzminecraft@gmail.com>
2025-05-17 19:44:20 -0400

    updated web scraper to be global and persistent

--- web_scraper.py
+++ web_scraper.py
@@ -9,20 +9,20 @@ from dotenv import load_dotenv
 
 driver = None
 
-def load_driver():
-    global driver
-    if driver is None:
+
+class Scraper:
+    def __init__(self):
         firefox_options = Options()
         firefox_options.add_argument("--headless")
         firefox_options.add_argument("--disable-gpu")
         firefox_options.add_argument("--no-sandbox")
         firefox_options.add_argument("--disable-dev-shm-usage")
 
-        driver = webdriver.Firefox(options=firefox_options)
-        driver.get("https://app.jamhacks.ca/social/")
+        self.driver = webdriver.Firefox(options=firefox_options)
+        self.driver.get("https://app.jamhacks.ca/social/")
 
         try:
-            driver.add_cookie({
+            self.driver.add_cookie({
                 "name": "__Secure-next-auth.session-token",
                 "value": os.getenv("__SECURE_NEXT_AUTH_SESSION_TOKEN"),
                 "domain": "app.jamhacks.ca",
@@ -34,33 +34,28 @@ def load_driver():
             print(f"failed to set cookie: {e}")
 
         print("DRIVER LOADED!!")
-    else:
-        print("driver already exists")
 
-    return driver
+    def get_jamhacks_data(self, jamhacks_code):
+        print("loading!")
+        load_dotenv()
 
+        print(driver)
+        self.driver.get("https://app.jamhacks.ca/social/" + str(jamhacks_code))
 
-def get_jamhacks_data(jamhacks_code):
-    print("loading!")
-    load_dotenv()
+        try:
+            name_element = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.TAG_NAME, "h1"))
+            )
+            name = name_element.text
+            print(name)
 
-    load_driver()
-    driver.get("https://app.jamhacks.ca/social/" + str(jamhacks_code))
+            social_elements = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_all_elements_located((By.TAG_NAME, "p"))
+            )
+            socials = [social.text for social in social_elements if len(social.text) != 0]
+            print(socials)
 
-    try:
-        name_element = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.TAG_NAME, "h1"))
-        )
-        name = name_element.text
-        print(name)
+            return name, socials
 
-        social_elements = WebDriverWait(driver, 10).until(
-            EC.presence_of_all_elements_located((By.TAG_NAME, "p"))
-        )
-        socials = [social.text for social in social_elements if len(social.text) != 0]
-        print(socials)
-
-        return name, socials
-
-    except Exception as e:
-        print(f"Error: {e}")
+        except Exception as e:
+            print(f"Error: {e}")
