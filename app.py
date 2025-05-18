1b3bffb05d6a5d6b2353beb76647590ca2404ac1
PenguinPush <icyzminecraft@gmail.com>
2025-05-18 04:23:57 -0400

    debug mode

--- app.py
+++ app.py
@@ -583,4 +583,4 @@ def get_user_json():
 
 if __name__ == "__main__":
     port = int(os.getenv('PORT', 5002))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=False)
